56ee141f7255f58ae8b9fe56c9486f4516b6aaa7
Jasen Carroll <108644550+jasencarroll@users.noreply.github.com>
2024-11-10 18:20:05 -0500

    user profile page

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,13 +1,30 @@
-import { useParams } from 'react-router-dom';
-import Body from '../components/Body';
+import { useState, useEffect } from "react";
+import { Stack } from "react-bootstrap";
+import Image from "react-bootstrap";
+import Spinner from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import Body from "../components/Body";
+import TimeAgo from "../components/TimeAgo";
+import { useApi } from "../contexts/ApiProvider";
 
 export default function UserPage() {
   const { username } = useParams();
+  const [user, setUser] = useState();
+  const api =  useApi();
+
+  useEffect(() => {
+    (async () => {
+      const response = await api.get('/users/' + username);
+      setUser(response.ok ? response.body : null);
+    })();
+  }, [username, api]);
 
   return (
     <Body sidebar>
-      <h1>{username}</h1>
-      <p>TODO</p>
+      {user == undefined ?
+        <Spinner animation="border" />
+        
+        
     </Body>
-  );
+  )
 }
